a28eb27e1e2b32858dca10c4f235b8f64b0e8a45
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-07-18 20:09:54 +0200

    Bumped version to v2.2.1.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ rtd_url =     "https://" + projectName + ".readthedocs.io/en/latest/"
 
 setuptools.setup(
 	name=projectName,
-	version="2.2.0",
+	version="2.2.1",
 
 	author="Patrick Lehmann",
 	author_email="Paebbels@gmail.com",
